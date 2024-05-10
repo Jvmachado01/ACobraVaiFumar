66035a1150d863195e3c602173fc9b20a114230a
Jvmachado01 <joaommachado89@outlook.com>
2024-05-09 22:22:56 -0300

    TiposDados

---
+++ 01Intro/01TiposDeDados.py
@@ -0,0 +1,27 @@
+# int
+idade = 25
+# float
+altura = 1.74
+# String
+nome = "John"
+# boolean: True, False
+maior_de_idade = True
+# None representa valor nulo ou ausente
+valor = None
+# list: (sequência mutáveis de elemtnos)
+numerosList = [1, 2, 3, 4, 5]
+# tuple: (similiar a lista, mas é imutável)
+coordenadasTuple = (10, 20)
+# dict: diciońario que contem conjuntos de chave-valor
+alunoDicionario = {"nome": "John", "idade": 25, "altura": 1.74}
+# set: conjunto não ordenada de elementos únicos
+coresColecao = {"vermelho", "azul", "verde"}
+
+print("idade:", idade, "altura:", altura, "nome:", nome, "maior de idade:", maior_de_idade)
+print("Qual valor:", valor)
+print("list:", numerosList)
+print("tuple:", coordenadasTuple)
+print("dict:", alunoDicionario)
+print("set:", coresColecao)
+
+
