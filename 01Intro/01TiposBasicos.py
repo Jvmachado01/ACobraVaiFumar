74fe057c6a46e89cd90f34e5a95ea501d702bd6f
Jvmachado01 <joaommachado89@outlook.com>
2024-05-09 21:14:19 -0300

    tipo

---
+++ 01Intro/01TiposBasicos.py
@@ -0,0 +1,9 @@
+a = 1 # integer
+b = 1.0 # decima (floating point)
+isTrue = True # boolean: True, False
+cadeiacaracteres = "Hello" + " " + "World!" # string
+
+print(a, type(a))
+print(b, type(b))
+print(isTrue, type(isTrue))
+print(cadeiacaracteres, type(cadeiacaracteres))
